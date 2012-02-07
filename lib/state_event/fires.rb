43b2f45b3ca20bea72a56fe8160eac678845c645
Brian Leonard <brian@bleonard.com>
2012-02-06 18:17:42 -0800

    combine methods in state fires

--- lib/state_event/fires.rb
+++ lib/state_event/fires.rb
@@ -26,57 +26,6 @@ module StateEvent
           return false
         end
 
-        fires(event_type, opts)
-      end
-      
-      def acts_as_state_event(opts={})
-        ::StateEvent::Config.event_class = self
-      end
-      
-      def acts_as_aasm_object(opts={})
-        unless opts.has_key?(:no_state)
-          include AASM
-          aasm_column :state
-        end
-
-        define_method(:suppress_state_events) do
-          @suppress_state_events = true
-        end
-        define_method(:enable_state_events) do
-          @suppress_state_events = false
-        end
-        define_method(:suppress_state_events?) do
-          !!@suppress_state_events
-        end
-
-
-
-        if opts.has_key?(:time)
-          method_name = :"default_event_time"
-          define_method(method_name) do
-            return send(opts[:time])
-          end
-        end
-        
-        if opts.has_key?(:actor)
-          method_name = :"default_event_actor"
-          define_method(method_name) do
-            return self if opts[:actor] == :self
-            return nil if opts[:actor] == false
-            return send(opts[:actor])
-          end
-        end
-
-        update_attr = :state_changed_at
-        send("before_save", "update_#{update_attr}", :if => :state_changed?)
-        define_method("update_#{update_attr}") do
-          send("#{update_attr}=", Time.now) if has_attribute?(update_attr)
-          true
-        end
-      end
-
-      def fires(event_type, opts)
- 
         opts[:subject] = :self unless opts.has_key?(:subject)
  
         method_name = :"fire_#{event_type}_after_save"
@@ -112,6 +61,50 @@ module StateEvent
         end
  
         after_save method_name, :if => opts[:if]
+      end
+      
+      def acts_as_state_event(opts={})
+        ::StateEvent::Config.event_class = self
+      end
+      
+      def acts_as_aasm_object(opts={})
+        unless opts.has_key?(:no_state)
+          include AASM
+          aasm_column :state
+        end
+
+        define_method(:suppress_state_events) do
+          @suppress_state_events = true
+        end
+        define_method(:enable_state_events) do
+          @suppress_state_events = false
+        end
+        define_method(:suppress_state_events?) do
+          !!@suppress_state_events
+        end
+
+        if opts.has_key?(:time)
+          method_name = :"default_event_time"
+          define_method(method_name) do
+            return send(opts[:time])
+          end
+        end
+        
+        if opts.has_key?(:actor)
+          method_name = :"default_event_actor"
+          define_method(method_name) do
+            return self if opts[:actor] == :self
+            return nil if opts[:actor] == false
+            return send(opts[:actor])
+          end
+        end
+
+        update_attr = :state_changed_at
+        send("before_save", "update_#{update_attr}", :if => :state_changed?)
+        define_method("update_#{update_attr}") do
+          send("#{update_attr}=", Time.now) if has_attribute?(update_attr)
+          true
+        end
       end
     end
   end
